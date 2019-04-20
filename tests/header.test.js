fd5dce1ae6dd609560121dade73075c62bc9c4db
himanshush200599 <himanshush214@gmail.com>
2019-04-20 06:22:52 -0400

    Started with JEST testing ,done with automated browser testing

---
+++ tests/header.test.js
@@ -0,0 +1,28 @@
+const puppeteer = require("puppeteer");
+
+let browser, page;
+
+beforeEach(async () => {
+  browser = await puppeteer.launch({
+    headless: false
+  });
+  page = await browser.newPage();
+  await page.goto("localhost:3000");
+});
+
+afterEach(async () => {
+  await browser.close();
+});
+
+test("Header has the correct text", async () => {
+  const text = await page.$eval("a.brand-logo", el => el.innerHTML);
+
+  expect(text).toEqual("Blogster");
+});
+
+test("Clicking login for O-Auth flow", async () => {
+  await page.click(".right a");
+  const url = await page.url();
+
+  expect(url).toMatch(/accounts\.google\.com/);
+});
