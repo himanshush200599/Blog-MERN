a655601b96b9151b60cbe9a634f8cd13b586617a
himanshush200599 <himanshush214@gmail.com>
2019-05-02 13:25:10 -0400

    Done with authentication testing

--- tests/header.test.js
+++ tests/header.test.js
@@ -26,3 +26,28 @@ test("Clicking login for O-Auth flow", async () => {
 
   expect(url).toMatch(/accounts\.google\.com/);
 });
+
+test("When signed in,shows logout button", async () => {
+  const id = "5cb81c109bba97431fb04b89";
+
+  const Buffer = require("safe-buffer").Buffer;
+  const sessionObject = {
+    passport: {
+      user: id
+    }
+  };
+  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString(
+    "base64"
+  );
+  const Keygrip = require("keygrip");
+  const keys = require("../config/keys");
+  const keygrip = new Keygrip([keys.cookieKey]);
+  const sig = keygrip.sign("session=" + sessionString);
+  await page.setCookie({ name: "session", value: sessionString });
+  await page.setCookie({ name: "session.sig", value: sig });
+  await page.goto("localhost:3000");
+  await page.waitFor('a[href="/auth/logout"]');
+
+  const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+  expect(text).toEqual("Logout");
+});
