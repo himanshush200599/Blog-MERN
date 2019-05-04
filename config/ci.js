0c21a8ecaf7d610134ec6a62769d2b42e4df496f
himanshush200599 <himanshush214@gmail.com>
2019-05-04 02:42:12 -0400

    adding CI server using travis

--- config/ci.js
+++ config/ci.js
@@ -2,7 +2,7 @@ module.exports = {
   googleClientID:
     "964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com",
   googleClientSecret: "KnH-rZC23z4fr2CN4ISK4srN",
-  mongoURI: "mongodb://blog_admin:himanshu20@ds031903.mlab.com:31903/blog_dev",
+  mongoURI: "mongodb://127.0.0.1:27017/blog_ci",
   cookieKey: "123123123",
   redisUrl: "redis://127.0.0.1:6347"
 };
