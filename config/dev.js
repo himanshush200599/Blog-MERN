a1c392d70c4ba7ba02ee0bc284442e7f77d594a0
himanshush200599 <himanshush214@gmail.com>
2019-05-30 23:14:57 +0530

    Done with Image upload using AWS S3

--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,7 @@ module.exports = {
   googleClientSecret: "KnH-rZC23z4fr2CN4ISK4srN",
   mongoURI: "mongodb://blog_admin:himanshu20@ds031903.mlab.com:31903/blog_dev",
   cookieKey: "123123123",
-  redisUrl: "redis://127.0.0.1:6379"
+  redisUrl: "redis://127.0.0.1:6379",
+  accessKeyId: "AKIAVDT7UG7X6WRDBNKI",
+  secretAccessKey: "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"
 };
